61784a96221f1145f21db60ecaaec982052fee44
MoonHunter233 <syhbb233@gmail.com>
2024-11-30 01:10:49 +0800

    update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,23 @@
-require'nvim-treesitter.configs'.setup {
+require("nvim-treesitter.configs").setup({
   -- 添加不同语言
-  ensure_installed = { "vim", "bash", "c", "cpp", "java", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
+  ensure_installed = {
+    "vim",
+    "bash",
+    "c",
+    "cpp",
+    "java",
+    "javascript",
+    "json",
+    "lua",
+    "python",
+    "typescript",
+    "tsx",
+    "css",
+    "rust",
+    "markdown",
+    "markdown_inline",
+    "ini",
+  }, -- one of "all" or a list of languages
 
   highlight = { enable = true },
   indent = { enable = true },
@@ -10,5 +27,5 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
     extended_mode = true,
     max_file_lines = nil,
-  }
-}
+  },
+})
