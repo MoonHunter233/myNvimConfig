3a4d031d687d9c128f258eae1a3cb777c7a68ecf
MoonHunter233 <syhbb233@gmail.com>
2024-11-30 23:17:56 +0800

    font style of code

---
+++ lua/fontStyle/init.lua
@@ -0,0 +1,11 @@
+-- 设置注释为斜体
+vim.cmd('highlight Comment gui=italic cterm=italic')
+
+-- 设置字符串为斜体
+vim.cmd('highlight String gui=italic cterm=italic')
+
+-- 设置关键字为粗体
+vim.cmd('highlight Keyword gui=bold cterm=italic')
+
+-- 设置函数名为斜体
+vim.cmd('highlight Function gui=bold cterm=italic')
