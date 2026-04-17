0269a753f1775c3d213f1c27d0997fcfe707b5ed
MoonHunter233 <syhbb233@gmail.com>
2026-04-17 16:25:51 +0800

    fix cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,14 +2,17 @@ return {
 	{
 		"hrsh7th/nvim-cmp",
 		event = "InsertEnter",
+		dependencies = {
+			{
+				"L3MON4D3/LuaSnip",
+				build = "make install_jsregexp",
+			},
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-buffer",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
 		config = require("plugins.cmp-config.cmp"),
 	},
-
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "hrsh7th/cmp-path" },
-	{ "hrsh7th/cmp-buffer" },
-
-	{ "L3MON4D3/LuaSnip" },
-	{ "saadparwaiz1/cmp_luasnip" },
-	{ "rafamadriz/friendly-snippets" },
 }
